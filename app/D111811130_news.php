3e29612113538670c3e7dff5e90a693c3b367160
ilham2025 <ilhamabdulaziz2020@gmail.com>
2021-04-19 22:49:57 +0700

    d111811130_laravel

---
+++ app/D111811130_news.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class D111811130_news extends Model
+{
+    protected $fillable = [
+    	'title','img_url','sub_desc','desc'];
+}
